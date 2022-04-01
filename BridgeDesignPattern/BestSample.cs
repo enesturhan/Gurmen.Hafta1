6b1bbe145a8f3844f34cc419f1ac5cac6b72cb58
enes turhan <enesturhan9@gmail.com>
2022-04-01 17:27:03 +0300

    updated

---
+++ BridgeDesignPattern/BestSample.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeDesignPattern
+{
+    /// <summary>
+    /// Örneğin elimize vehicle(araç) classımız olsun Bu class birden çok taşıt türü alabilir örneğin motor,araba,uçak,kamyon,tren,tramvay gibi ve bunların yaktığı yakıt türleri de birbirinden farklı olucaktır.
+    /// Ve bunun gibi birden fazla özellik bizim base sınıfımızdan türetildiği zaman sonsuz sayıda kombinason içerecektir.Bu yüzden soyut nesneleri kullanarak(interface,abstract) birbirleri ile yakın ilişkili sınıfları, birbirinden bağımsız olarak geliştirme yaparak bunları farklı hiyarerşilere bölmeliyiz.
+    ///Burada bir ıfuel interfacemiz yardımı ile diesel ve gazoline sınıflarımıza implemente ettik.    Sonrasında abstract olarak motorbike sınıfımızı olusturduk burada ıfuel interfacesi ile köprü görevinde kullandık ve birbirine bağlamış olduk.
+    ///Sonrasında ise 2 adet motor class olusturarak bunları base constructura yönlendirmiş olduk.
+    /// </summary>
+    internal class BestSample
+    {
+
+
+        public interface IFuelType
+        {
+
+            public string FuelType();
+
+        }
+
+        public class DieselFuel : IFuelType
+        {
+            public string FuelType()
+            {
+                return "Diesel";
+            }
+        }
+        public class GazolineFuel : IFuelType
+        {
+            public string FuelType()
+            {
+                return "Gazoline";
+            }
+        }
+
+        public abstract class Motorbike
+        {
+            private readonly IFuelType _fuelType;
+
+           
+
+            public string bikeName { get; set; }
+            public Motorbike(IFuelType fuelType,string BikeName)
+            {
+                _fuelType = fuelType;
+                bikeName = BikeName;
+
+            }
+          
+            public void Drive()
+            {
+                Console.WriteLine("Bu araç yakıtı" +" "+_fuelType.FuelType()+" "+"ismi ise" +" "+bikeName);
+            }
+          
+        }
+
+        public class StreetMotorBike : Motorbike
+        {
+            public StreetMotorBike(IFuelType fuelType, string bikeName) : base(fuelType, bikeName)
+            {
+
+            }
+
+         
+        }
+        public class RacingMotorBike : Motorbike
+        {
+            public RacingMotorBike(IFuelType fuelType, string bikeName) : base(fuelType, bikeName)
+            {
+            }
+
+        
+        }
+
+        class BridgeDesignPattern
+        {
+            static void Main(string[] args)
+            {
+                StreetMotorBike streetMotor = new StreetMotorBike(new DieselFuel(), "Sokak Motoru");
+                streetMotor.Drive();
+                RacingMotorBike racingMotorBike = new RacingMotorBike(new GazolineFuel(), "Yarış Motoru");
+                racingMotorBike.Drive();
+            }   
+        }
+    }
+
+        
+} 
---
+++ FacadeDesignPattern/BestSample.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static FacadeDesignPattern.BestSample;
+
+namespace FacadeDesignPattern
+{
+    /// <summary>
+    /// Bir sistemde parçaları olusturan classları istemciden soyutlayarak kullanımı daha kolaylastırmak için kullanılan tasarım kalıbıdır.
+    /// Katmanlar arası bağımlılıgı azaltmak icin kullanılır. Soyutlamayı saglar ve kullanıcıya bir arayüz sunar.
+    
+    /// birden fazla classı tek bir classta cagırarak karmaşıklıgı önlemek ve bize tek elden hizmet sunar.
+    /// Burada bir agile yönetim sürecinde projeye atılan kullanıcıların durumlarını uygunluklarını en sonda facede classı ile kontrol sağlamıs olduk.
+    /// </summary>
+    internal class BestSample
+    {
+
+        public class User
+        {
+            public string UserName { get; set; }
+
+            public string Email { get; set; }
+
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+
+
+        }
+
+        class Project
+        {
+            public bool IsAssign(User email)
+            {
+                
+                if (email == null)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        class Contributor
+        {
+            public bool IsContributor(User user)
+            {
+                if(user.Email != null)
+                {
+                    return true;
+                }
+                return false;
+            }
+
+        }
+
+        class Sprint
+        {
+            public string SprintName { get; set; }
+            public int SprintCount { get; set; }
+            public int GetSprint(int count)
+            {
+                if (string.IsNullOrEmpty(SprintName))
+                {
+                    throw new Exception("isim giriniz");
+                }
+                if (SprintCount > 4)
+                {
+                    return 4;
+                }
+                return 2;
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// Facede sınıfında istenilen kısımları organize ederek tek bir sınıf yardımıyla bize sunar.
+        /// </summary>
+        class Facede
+        {
+            private readonly Contributor _contributor;
+            private readonly Sprint _sprint;
+            private readonly Project _project;
+
+            public Facede(Contributor contributor,Sprint sprint,Project project)
+            {
+                _contributor = contributor;
+                _sprint = sprint;
+                _project = project;
+            }
+            
+            public bool IsAssignable(User user)
+            {
+                if (_sprint.SprintCount > 4)
+                {
+                    return false;
+                }
+                if (_contributor.IsContributor(user) != false)
+                {
+                    return false;
+                }
+                var value =_project.IsAssign(user);
+                if (value)
+                {
+                    return false;
+                }
+
+                return true;
+                Console.WriteLine("Kisi projeye atanabilir");
+            }
+          
+
+        }
+
+
+
+    }
+}
